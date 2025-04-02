86589c816d097de38f828747eff7a4f2992917c6
Shineunju <suj9810@naver.com>
2025-04-02 13:12:39 +0900

    Test: git remote test

---
+++ src/test/java/com/example/calendardevelop/CalendarDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calendardevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
