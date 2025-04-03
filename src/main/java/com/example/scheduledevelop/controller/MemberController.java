d357c7bc8c6730ecc75ee1d53d1c80300c90253a
Shineunju <suj9810@naver.com>
2025-04-03 09:57:52 +0900

    Feat: 특정 회원 조회 구현

--- src/main/java/com/example/scheduledevelop/controller/MemberController.java
+++ src/main/java/com/example/scheduledevelop/controller/MemberController.java
@@ -1,15 +1,13 @@
 package com.example.scheduledevelop.controller;
 
+import com.example.scheduledevelop.dto.MemberResponseDto;
 import com.example.scheduledevelop.dto.SignUpRequestDto;
 import com.example.scheduledevelop.dto.SignUpResponseDto;
 import com.example.scheduledevelop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
@@ -24,4 +22,9 @@ public class MemberController {
         return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponseDto> findById(@PathVariable Long id) {
+        MemberResponseDto memberResponseDto = memberService.findById(id);
+        return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/scheduledevelop/service/MemberService.java
+++ src/main/java/com/example/scheduledevelop/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.example.scheduledevelop.service;
 
+import com.example.scheduledevelop.dto.MemberResponseDto;
 import com.example.scheduledevelop.dto.SignUpResponseDto;
 import com.example.scheduledevelop.entity.Member;
 import com.example.scheduledevelop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,4 +24,12 @@ public class MemberService {
     }
 
 
+    public MemberResponseDto findById(Long id) {
+        Optional<Member> optionalMember = memberRepository.findById(id);
+        if (optionalMember.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exists id : " + id);
+        }
+        Member findMember = optionalMember.get();
+        return new MemberResponseDto(findMember.getUsername(), findMember.getEmail());
+    }
 }
